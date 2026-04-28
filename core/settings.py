5e6f6056d91f06885857152a949ad70104eb3320
Mamun <beparimamun708@gmail.com>
2026-04-28 22:50:21 +0600

    Fixed settings for Production

--- core/settings.py
+++ core/settings.py
@@ -14,12 +14,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-default-key-for-dev')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+# এটি পরিবর্তন করুন
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
 
 
 # Application definition
@@ -34,6 +35,7 @@ INSTALLED_APPS = [
 
     'rest_framework',
     'corsheaders',
+    'django_celery_results',
     'chat', # Our chat app
 ]
 
@@ -139,3 +141,5 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 AUTH_USER_MODEL = 'chat.user'
+
+CORS_ALLOW_ALL_ORIGINS = True  # Development 
