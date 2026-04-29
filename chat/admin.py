6c9aa82415d9be3ea4e73bccdba57569a26a15ef
Mamun <beparimamun708@gmail.com>
2026-04-30 01:13:49 +0600

    Added admin.py with model registered

--- chat/admin.py
+++ chat/admin.py
@@ -1,3 +1,17 @@
-from django.contrib import admin
+from django.contrib import admin 
+from .models import User, Conversation, Message
 
 # Register your models here.
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+   list_display = ['id', 'username', 'email', 'phone_number', 'is_online']
+
+@admin.register(Conversation)
+class ConversationAdmin(admin.ModelAdmin):
+   list_display = ['id', 'is_group_chat', 'created_at']
+
+@admin.register(Message)
+class MessageAdmin(admin.ModelAdmin):
+   list_display = ['id', 'conversation', 'sender', 'timestamp', 'is_read']
+
+   
