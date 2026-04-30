353be4631820d760b1849df8da238030775f479b
Mamun <beparimamun708@gmail.com>
2026-04-30 10:56:32 +0600

    Implement JWT Authentication, Generate api/token/ and api/refresh/token for registration and login

--- chat/views.py
+++ chat/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from .admin import User
+from .serializers import RegisterSerializer
+from rest_framework import generics
+from rest_framework.permissions import AllowAny
 
 # Create your views here.
+class RegisterView(generics.CreateAPIView):
+   queryset = User.objects.all()
+   serializer_class = RegisterSerializer
+   permission_classes = (AllowAny,)
+   
